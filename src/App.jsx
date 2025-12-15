8984c9ddcd7b1c46f5dacdd4e6f4bc5e9403a2d0
HossamGezo <ha2ghossam10@gmail.com>
2025-12-15 14:40:05 +0200

    chore(deps): install core dependencies (react 18, redux 4, react-redux 8)

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Cake & Icecream Store</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+// - - - - - - - - - - React Library
+import {createRoot} from "react-dom/client";
+// - - - - - - - - - - Import Component Files
+import App from "./App.jsx";
+// - - - - - - - - - - Import Styles Files
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(<App />);
